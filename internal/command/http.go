4aae61f99f8671d60397485b585b3a52714c28be
Camden DeVoogd <camdendevoogd@gmail.com>
2024-10-09 09:46:02 -0600

    Add ability to set header(s) and method

--- internal/command/http.go
+++ internal/command/http.go
@@ -5,14 +5,34 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const headerDelimiter = ":"
+
 type HTTP struct {
-	URL string `arg:"" name:"url" help:"the URL to send a request to"`
+	URL     string   `arg:"" name:"url" help:"The URL to send a request to"`
+	Method  string   `name:"method" short:"M" default:"GET" enum:"${http_methods}"`
+	Headers []string `name:"header" short:"H" help:"The header(s) to add to the request" placeholder:"'Header: Value'" sep:"none"`
+
+	headers http.Header
+}
+
+func (cmd *HTTP) AfterApply() error {
+	cmd.headers = http.Header{}
+	for _, header := range cmd.Headers {
+		parts := strings.SplitN(header, headerDelimiter, 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("header %q is malformed: missing %q delimiter", header, headerDelimiter)
+		}
+		cmd.headers.Add(parts[0], strings.TrimSpace(parts[1]))
+	}
+
+	return nil
 }
 
 func (cmd *HTTP) Run() error {
-	req, err := http.NewRequest(http.MethodGet, cmd.URL, http.NoBody)
+	req, err := cmd.buildRequest()
 	if err != nil {
 		return fmt.Errorf("error building request: %w", err)
 	}
@@ -25,4 +45,14 @@ func (cmd *HTTP) Run() error {
 
 	_, err = io.Copy(os.Stdout, resp.Body)
 	return err
+}
+
+func (cmd *HTTP) buildRequest() (*http.Request, error) {
+	req, err := http.NewRequest(cmd.Method, cmd.URL, http.NoBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header = cmd.headers
+	return req, nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/alecthomas/kong"
 	"github.com/cdevoogd/conndebug/internal/command"
 )
 
+var supportedHTTPMethods = []string{
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodPut,
+	http.MethodPost,
+	http.MethodPatch,
+	http.MethodDelete,
+}
+
 func main() {
 	rootCommand := command.Root{}
-	ctx := kong.Parse(&rootCommand)
+	ctx := kong.Parse(&rootCommand, kong.Vars{
+		"http_methods": strings.Join(supportedHTTPMethods, ","),
+	})
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
 }
